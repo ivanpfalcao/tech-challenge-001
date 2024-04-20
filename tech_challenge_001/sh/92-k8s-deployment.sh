f0e628ee32f58157b79aa3697984caac08fc7054
Ivan Falcao <ivanpfalcao@outlook.com>
2024-04-20 19:22:12 -0300

    K8s service and deployment

---
+++ tech_challenge_001/sh/92-k8s-deployment.sh
@@ -0,0 +1,6 @@
+BASEDIR=$(cd -P -- "$(dirname -- "${0}")" && pwd -P)
+
+NAMESPACE="tech-challenge-001"
+
+kubectl create namespace "${NAMESPACE}"
+kubectl -n "${NAMESPACE}" apply -f "${BASEDIR}/tech-challenge-dpl.yaml"
